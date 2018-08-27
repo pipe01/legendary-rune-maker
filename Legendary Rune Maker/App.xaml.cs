7f9a3d0c0f034960af1c9145782f48e8e55edf64
Felipe Martinez <pipernene@gmail.com>
2018-08-27 18:50:14 +0200

    Fix buttons not being clickable

--- Legendary Rune Maker/App.xaml.cs
+++ Legendary Rune Maker/App.xaml.cs
@@ -15,12 +15,8 @@ namespace Legendary_Rune_Maker
     {
         private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            (sender as Window)?.DragMove();
-        }
-
-        private void Minimize_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-
+            if (sender == e.Source)
+                (sender as Window)?.DragMove();
         }
     }
 }
