583a54053493c2b193f175ba73eaa8223639f918
Felipe Martinez <pipernene@gmail.com>
2018-08-27 23:02:52 +0200

    Added UploadOnLock setting

--- Legendary Rune Maker/Properties/Settings.Designer.cs
+++ Legendary Rune Maker/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace Legendary_Rune_Maker.Properties {
                 this["AutoAccept"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool UploadOnLock {
+            get {
+                return ((bool)(this["UploadOnLock"]));
+            }
+            set {
+                this["UploadOnLock"] = value;
+            }
+        }
     }
 }
