80564d3c71c898abb8bb6b6ce2d5b5f2d7dfa0cd
Felipe Martinez <pipernene@gmail.com>
2018-09-01 15:40:58 +0200

    Change UI language though config file

--- Legendary Rune Maker/App.xaml.cs
+++ Legendary Rune Maker/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Legendary_Rune_Maker.Data;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,11 @@ namespace Legendary_Rune_Maker
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = Config.Default.Culture;
+        }
+
         private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             if (sender == e.Source)
