eee83178f16704fb8d4876e9811a6cd548318228
Felipe Martinez <pipernene@gmail.com>
2018-09-02 01:33:30 +0200

    Made LoLButton a custom control for better content displaying

---
+++ Legendary Rune Maker/Utils/UppercaseConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Legendary_Rune_Maker.Utils
+{
+    public class UppercaseConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as string)?.ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => new UppercaseConverter();
+    }
+}
