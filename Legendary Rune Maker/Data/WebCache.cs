9ff3160f531952265901e7b8bfc6b15705f1938f
Felipe Martinez <pipernene@gmail.com>
2018-09-01 14:32:04 +0200

    Added cache class

---
+++ Legendary Rune Maker/Data/WebCache.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legendary_Rune_Maker.Data
+{
+    internal static class WebCache
+    {
+        private static IDictionary<string, string> FileCache = new Dictionary<string, string>();
+
+        private static WebClient Client => new WebClient { Encoding = Encoding.UTF8 };
+
+        public static async Task<string> String(string url, WebClient client = null)
+        {
+            if (!FileCache.TryGetValue(url, out var value))
+            {
+                value = await (client ?? Client).DownloadStringTaskAsync(url);
+            }
+
+            return value;
+        }
+
+        public static async Task<T> Json<T>(string url, WebClient client = null)
+        {
+            return JsonConvert.DeserializeObject<T>(await String(url, client));
+        }
+    }
+}
