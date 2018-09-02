fc2b3039b93d0b33c286d4596a038828773539f2
Felipe Martinez <pipernene@gmail.com>
2018-09-02 18:39:36 +0200

    Completed Champion.GG provider

--- Legendary Rune Maker/Data/Rune providers/ChampionGGProvider.cs
+++ Legendary Rune Maker/Data/Rune providers/ChampionGGProvider.cs
@@ -52,9 +52,30 @@ namespace Legendary_Rune_Maker.Data.Rune_providers
             return ret.ToArray();
         }
 
-        protected override Task<RunePage> GetRunePageInner(int championId, Position position)
+        protected override async Task<RunePage> GetRunePageInner(int championId, Position position)
         {
-            throw new NotImplementedException();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(await new WebClient().DownloadStringTaskAsync(GetChampionURL(championId, position)));
+
+            var pathIcons = doc.DocumentNode.SelectNodes("//img[contains(@class, 'PathButton__Icon')]").Take(2);
+            var pathStyles = pathIcons.Select(GetPathStyleId).ToArray();
+
+            var perkIcons = doc.DocumentNode.SelectNodes("//div[contains(@class, 'LeftSide')]//img[contains(@class, 'PerkButton__Icon')]").Take(6);
+            var perkIds = perkIcons.Select(GetPerkId).ToArray();
+
+            return new RunePage(perkIds, pathStyles[0], pathStyles[1], championId, position);
+        }
+
+        private static int GetPerkId(HtmlNode node)
+        {
+            string src = node.GetAttributeValue("src", "");
+            return Riot.GetAllRunes().Values.First(o => src.Contains(o.IconURL)).ID;
+        }
+
+        private static int GetPathStyleId(HtmlNode node)
+        {
+            string src = node.GetAttributeValue("src", "");
+            return Riot.GetRuneTrees().Result.First(o => src.Contains(o.IconURL)).ID;
         }
     }
 }
