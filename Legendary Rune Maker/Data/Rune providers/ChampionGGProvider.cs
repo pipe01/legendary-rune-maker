6f663634d16bfb1f32927bebf76992c1f4bc9ecf
Felipe Martinez <pipernene@gmail.com>
2018-09-02 17:58:56 +0200

    Created Champion.GG rune provider

---
+++ Legendary Rune Maker/Data/Rune providers/ChampionGGProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legendary_Rune_Maker.Data.Rune_providers
+{
+    internal class ChampionGGProvider : RuneProvider
+    {
+        public override string Name => "Champion.GG";
+
+        protected override Task<Position[]> GetPossibleRolesInner(int championId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<RunePage> GetRunePageInner(int championId, Position position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
