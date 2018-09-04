834beb2bc8df4d17437e5f365a40cf1ffef01707
Felipe Martinez <pipernene@gmail.com>
2018-09-04 15:01:48 +0200

    Move update manifest

--- Legendary Rune Maker/LoadingWindow.xaml.cs
+++ Legendary Rune Maker/LoadingWindow.xaml.cs
@@ -94,7 +94,7 @@ namespace Legendary_Rune_Maker
             Progress.IsIndeterminate = true;
 
             var manager = new UpdateManager(
-                new WebPackageResolver("https://pipe0481.heliohost.org/plrm.man"),
+                new WebPackageResolver("https://pipe0481.heliohost.org/plrm/updates.man"),
                 new ZipPackageExtractor());
 
             var update = await await Task.WhenAny(Task.Delay(3000).ContinueWith<CheckForUpdatesResult>(_ => null), manager.CheckForUpdatesAsync());
