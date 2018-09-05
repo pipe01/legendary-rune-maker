ebf4f6c548c6a05aa3a5c69145c9623f9a9b22fa
Felipe Martinez <pipernene@gmail.com>
2018-09-05 17:17:09 +0200

    Added items retrieving

---
+++ Legendary Rune Maker/Data/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legendary_Rune_Maker.Data
+{
+    public struct Item
+    {
+        public int ID { get; }
+
+        public Item(int id)
+        {
+            this.ID = id;
+        }
+    }
+}
