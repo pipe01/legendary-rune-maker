ed6d9229daa656139177c345bfe3ab69cef954a6
Felipe Martinez <pipernene@gmail.com>
2018-09-05 16:50:19 +0200

    Update LCU.NET

---
+++ Legendary Rune Maker/Data/ItemSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legendary_Rune_Maker.Data
+{
+    public class ItemSet
+    {
+        public class SetBlock
+        {
+            public string Name { get; set; }
+            public int[] Items { get; set; }
+        }
+
+        public int Champion { get; set; }
+        public Position Position { get; set; }
+
+        public SetBlock[] Blocks { get; set; }
+    }
+}
