0bde25712d08a758951495f44cdc4242be3a77e4
Felipe Martinez <pipernene@gmail.com>
2018-09-09 00:37:28 +0200

    Version 1.1.0

--- Legendary Rune Maker/Properties/AssemblyInfo.cs
+++ Legendary Rune Maker/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
